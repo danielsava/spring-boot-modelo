f818a9007b103c7fc6a69c5c551bcc285a057d36
Daniel Sava <danielsavas@gmail.com>
2020-02-05 19:26:06 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/savasoft/SpringBootModeloApplication.java
@@ -0,0 +1,13 @@
+package br.com.savasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModeloApplication.class, args);
+	}
+
+}
